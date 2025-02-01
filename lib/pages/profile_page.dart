d4322c20b44067c27bc1d412781bf0fb7b4e7dfc
YeonwooNuf <jangyw0927@naver.com>
2025-02-01 22:10:31 +0900

    // 활동 탭 사진 경로 수정

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -213,7 +213,7 @@ class _ProfilePageState extends State<ProfilePage>
                         if (post.imageUrl != null)
                           Expanded(
                             child: Image.network(
-                              'http://10.0.2.2:8080${post.imageUrl}', // 절대 경로로 변환
+                              'http://10.0.2.2:8080/${post.imageUrl}', // 절대 경로로 변환
                               fit: BoxFit.cover,
                               errorBuilder: (context, error, stackTrace) {
                                 return Icon(Icons.image_not_supported, color: Colors.white);
